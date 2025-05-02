cd9a73885cf187f00c15127e426a9615b6dd3f1b
hitsuji2001 <hitsuji2001@gmail.com>
2025-05-02 15:18:07 +0700

    Config `i3`, `kitty`, add backup feature `setup.sh`

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 ### Description
 
 A place to store all those dotfiles
+
+### Usage
+
+You can move config individually or you can use `setup.sh` file to create symlink to default location.
+
+
+| Target | Location         |
+| ------ | ---------------- |
+| emacs  | ~/.emacs.el      |
+| i3     | ~/.config/i3/    |
+| kitty  | ~/.config/kitty/ |
+| nvim   | ~/.config/nvim/  |
+| rofi   | ~/.config/rofi/  |
+| tmux   | ~/.tmux.conf     |
+| vim    | ~/.vimrc         |
+
+
+``` bash
+./setup all
+./setup vim i3 kitty
+```
--- setup.sh
+++ setup.sh
@@ -15,10 +15,73 @@ usage() {
     done
 }
 
-TARGET+='all'
+backup_and_remove() {
+    if [ $# -ne 1 ]; then
+	echo "[ERROR]: Must provide only 'destination' path"
+	exit 1
+    fi
+    backup_path="./backup"
+    dst=$1
+
+    if [ ! -d "$backup_path" ]; then
+	echo "[INFO]: Creating folder '$backup_path'"
+	mkdir "$backup_path"
+    fi
+    echo "[INFO]: Copy '$dst' to '$backup_path'"
+    cp -Lr "$dst" "$backup_path"
+    echo "[INFO]: Removing '$dst'"
+    rm -r "$dst"
+}
+
+link_src_to_dst() {
+    if [ $# -ne 2 ]; then
+	echo "[ERROR]: Must provide 'source' and 'destination'"
+	exit 1
+    fi
+    src=$1
+    dst=$2
+
+    if [ -d "$dst" ]; then
+	echo "[INFO]: Folder '$dst' exist. Backing up..."
+	backup_and_remove "$dst"
+    elif [ -f "$dst" ]; then
+	echo "[INFO]: File '$dst' exist. Backing up..."
+	backup_and_remove "$dst"
+    fi
+
+    echo "[INFO]: Creating symlink for '$1' -> '$2'"
+    ln -sf "$1" "$2"
+}
+
+setup_dotfiles() {
+    if [ $# -ne 1 ]; then
+	echo "[ERROR]: Expected only one target"
+	exit 1
+    fi
+    target=$1
+
+    case $target in
+	'i3')    link_src_to_dst "$(pwd)/$target/"           "$HOME/.config/$target";;
+	'kitty') link_src_to_dst "$(pwd)/$target/"           "$HOME/.config/$target";;
+	'rofi')  link_src_to_dst "$(pwd)/$target/"           "$HOME/.config/$target";;
+	'nvim')  link_src_to_dst "$(pwd)/$target/"           "$HOME/.config/$target";;
+	'vim')   link_src_to_dst "$(pwd)/$target/.vimrc"     "$HOME/.vimrc";;
+	'emacs') link_src_to_dst "$(pwd)/$target/README.org" "$HOME/.emacs.el";;
+	'tmux')  link_src_to_dst "$(pwd)/$target/tmux.conf"  "$HOME/.tmux.conf";;
+	'all') : ;;
+	*) 
+	    echo "[ERORR]: unimplemented target '$target'"
+	    exit 1
+	    ;;
+    esac
+}
+
+TARGET+=('all')
 for f in $(ls -d */); do
     folder=$(basename $f)
-    TARGET+=($folder)
+    if [ "$folder" != "backup" ]; then
+	TARGET+=($folder)
+    fi
 done
 
 if [ $ARGC -lt 1 ]; then
@@ -42,16 +105,13 @@ for arg in $ARGV; do
     fi
 done
 
-set -xe
-
 for arg in $ARGV; do
-    case $arg in
-	'emacs') ln -sf "$(pwd)/$arg/README.org" "$HOME/.emacs.el" ;;
-	'i3') ln -sf "$(pwd)/$arg/" "$HOME/.config/" ;;
-	'kitty') ln -sf "$(pwd)/$arg/" "$HOME/.config/" ;;
-	'vim') ln -sf "$(pwd)/$arg/.vimrc" "$HOME/.vimrc" ;;
-	'nvim') ln -sf "$(pwd)/$arg/" "$HOME/.config/" ;;
-	'tmux') ln -sf "$(pwd)/$arg/tmux.conf" "$HOME/.tmux.conf" ;;
-	*) echo "[ERORR]: unimplemented target '$arg'" ;;
-    esac
+    if [ "$arg" != "all" ]; then
+	setup_dotfiles $arg
+    else
+	for target in ${TARGET[@]}; do
+	    setup_dotfiles $target
+	done
+	break
+    fi
 done
